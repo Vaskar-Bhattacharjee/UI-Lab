d205c76bbbf56c3bc9fb0592121d3b31c868bf75
Vaskar-Bhattacharjee <vaskarbhattacharjee03@gmail.com>
2026-09-23 21:08:06 +0600

    refactor: change Bento_Section export to default and fix HTML entity in description

--- app/Components/ui/bento_section/page.tsx
+++ app/Components/ui/bento_section/page.tsx
@@ -1,7 +1,7 @@
 import { Bento } from "../bento/bento";
 
 
-export const Bento_Section = () => {
+export default function Bento_Section() {
     return (
     <div className="h-full min-h-screen w-screen flex flex-col items-center justify-center gap-8 bg-white py-10">
       <h1 className="font-inter font-medium tracking-tight text-4xl text-neutral-700 text-center">
@@ -10,7 +10,7 @@ export const Bento_Section = () => {
         An autonomous system
       </h1>
       <p className="text-neutral-500/80 text-center text-base max-w-lg px-4 lg:px-4">
-        Our agentic AI doesn't just respond. It thinks, plans, and takes action.
+        Our agentic AI doesn&apos;t just respond. It thinks, plans, and takes action.
         Built to handle real work, not just conversations.
       </p>
       <Bento />
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Bento_Section } from "./Components/ui/bento_section/page";
+import  Bento_Section  from "./Components/ui/bento_section/page";
 
 export default function Home() {
   return <Bento_Section />;
